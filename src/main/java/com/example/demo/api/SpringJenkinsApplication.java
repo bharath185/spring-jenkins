9b3a38417839ec464689aea87cecda54a1f6e647
bharathA1818 <abharathkumarindia@gmail.com>
2022-02-03 20:10:50 +0530

    first commit

---
+++ src/main/java/com/example/demo/api/SpringJenkinsApplication.java
@@ -0,0 +1,21 @@
+package com.example.demo.api;
+
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJenkinsApplication {
+public static org.slf4j.Logger Logger = LoggerFactory.getLogger(SpringJenkinsApplication.class);
+	
+	public void intt() {
+		Logger.info("Application Started....");
+	}
+	
+
+	public static void main(String[] args) {
+		Logger.info("Application Executed....");
+		SpringApplication.run(SpringJenkinsApplication.class, args);
+	}
+
+}
